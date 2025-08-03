5e5f64195b185fcc69e9647fd2d17c4fa47e82fc
presidentsnow <xaviyu2005@gmail.com>
2025-08-03 22:16:26 +0200

    Encapsulation and abstraction finished. Only remains polymorphism.

--- oop_cpp/beautyCode_oop.cpp
+++ oop_cpp/beautyCode_oop.cpp
@@ -9,13 +9,30 @@
 // data and functions that work on that data
 */
 
-class Employee
+class AbstractEmployee
+{
+    virtual void AskForPromotion() = 0;
+};
+
+class Employee:AbstractEmployee
 {
 private: // by default the class is private
-    std::string Name; // attribute
     std::string Company; // attribute
     int Age; // attribute
+protected:
+    std::string Name; // attribute
 public:
+    void AskForPromotion()
+    {
+        if (Age > 30)
+        {
+            std::cout << Name << " got promoted!" << std::endl;
+        }
+        else
+        {
+            std::cout << Name << " NO promotion for you!" << std::endl;
+        }
+    }
     // Info about setter
     /*
     // Setters are methods that allow you to modify the values
@@ -77,29 +94,86 @@ public:
     }
 };
 
+// inheritance OOP
+/*
+// Developer is a subclass or child class
+// Employee is a base class or father class
+*/
+class Developer : public Employee
+{
+public:
+    std::string FavProgrammingLanguage;
+    Developer(std::string name, std::string company, int age, std::string FavProgrammingLanguage)
+        :Employee(name, company, age) // constructor of the class
+    {
+        FavProgrammingLanguage = FavProgrammingLanguage;
+    };
+    void FixBug()
+    {
+        std::cout << Name << " fixed bug using " << FavProgrammingLanguage << std::endl;
+    }
+};
+
+class Teacher : public Employee
+{
+public:
+    std::string Subject;
+    void PrepareLesson()
+    {
+        std::cout << Name << " is preparing " << Subject << " lesson " << std::endl;
+    }
+
+    // constructor
+    Teacher(std::string name, std::string company, int age, std::string subject)
+        :Employee(name, company, age)
+    {
+        Subject = subject;
+    }
+};
+
 int main()
 {
-    Employee employee1 = Employee("Javi", "Nvidia", 18); // use the Constructor
+    Developer d = Developer("Gabriel", "Amazon", 28, "C++");
+    d.FixBug();
+    d.AskForPromotion();
+
+    Teacher t = Teacher("Josep", "Telefonica", 24, "Mathematics");
+    t.PrepareLesson();
+    t.AskForPromotion();
+
+    /*
+    // Employee employee1 = Employee("Javi", "Nvidia", 18); // use the Constructor
     //employee1.Name = "Javi";
     //employee1.Company = "Nvidia";
     //employee1.Age = 18;
-    employee1.IntroduceYourself();
+    // employee1.IntroduceYourself();
+    // employee1.AskForPromotion();
+    */
 
-    std::cout << "\n";
+    //std::cout << "\n";
 
-    Employee employee2 = Employee("David", "Amazon", 32); // another Constructor
-    //employee2.Name = "David";
-    //employee2.Company = "Amazon";
-    //employee2.Age = 32;
-    employee2.IntroduceYourself();
+    /*
+    // Employee employee2 = Employee("David", "Amazon", 32); // another Constructor
+    // employee2.Name = "David";
+    // employee2.Company = "Amazon";
+    // employee2.Age = 32;
+    // employee2.IntroduceYourself();
+    // employee2.AskForPromotion();
+    */
 
-    std::cout << "\n";
+    //std::cout << "\n";
 
-    Employee employee3 = Employee("Pepe", "Oracle", 27);
-    employee3.IntroduceYourself();
-    
-    employee1.setAge(25);
+    /*
+    // Employee employee3 = Employee("Pepe", "Oracle", 27);
+    // employee3.IntroduceYourself();
+    // employee3.AskForPromotion();
+    */
+
+    /*
+    // access the private elements
+    // employee1.setAge(25);
     // this stdout is: Jav is 25 years old
-    std::cout << employee1.getName() << " is " << employee1.getAge() << " years old " << std::endl;
+    // std::cout << employee1.getName() << " is " << employee1.getAge() << " years old " << std::endl;
+    */
     return 0;
 };
