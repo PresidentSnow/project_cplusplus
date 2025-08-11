363123c3a9dd600295d0bbb8d54fae4db8c28317
presidentsnow <xaviyu2005@gmail.com>
2025-08-11 22:43:21 +0200

    Alien vs Predator game iniciated...

--- programming_games/alien_vs_predator.cpp
+++ programming_games/alien_vs_predator.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <vector> // perform tasks on vectors
+#include <memory> // facilities for dynamic memory management
+#include <random> // generate random numbers
+#include <algorithm> // modify ranges of data from data structures
+#include <string> // perform tasks on strings
+#include <unordered_map> // provides an associative container that stores elements in key-value pairs
+
+// Battlefield Interface
+class IBattlefield
+{
+public:
+    virtual ~IBattlefield() = default; // virtual destructor
+    
+    // The following methods are declared as pure
+    // virtual functions, indicated by the = 0 syntax
+    virtual int ObjectCount() const = 0; // returns the number of objects in the battlefield
+    
+    // This allows access to a specific game object in the battlefield based on its index
+    virtual class IGameObject* GetObjectAt(int index) = 0;
+    
+    virtual void ExecuteTurn() = 0; // This method is intended to execute the logic for a turn in the game
+    
+    // This method likely serves as a visitor pattern implementation, allowing
+    // an external visitor to perform operations on the battlefield or its objects
+    virtual void Visit() = 0;
+};
+
+// Interface for game objects
+class IGameObject
+{
+    virtual ~IGameObject() = default;
+    virtual void ExecuteTurn(IBattlefield* battlefield) = 0;
+    virtual void AddLifePoints(int points) = 0;
+    virtual int GetAttack() const = 0;
+    virtual int GetDefense() const = 0;
+    virtual int GetId() const = 0;
+    virtual int GetLifePoints() const = 0;
+    virtual std::string GetType() const = 0;
+    virtual std::string GetStatus() const = 0;
+};
 
 int main()
 {
-    std::cout << "Hola\n";
     return 0;
 }
