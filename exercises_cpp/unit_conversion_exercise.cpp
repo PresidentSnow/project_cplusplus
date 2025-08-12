e6481395af996f922ccde33c75dde3980f3c4b56
presidentsnow <xaviyu2005@gmail.com>
2025-08-12 13:24:58 +0200

    US flag printed done and also unit conversion exercises done.

--- exercises_cpp/unit_conversion_exercise.cpp
+++ exercises_cpp/unit_conversion_exercise.cpp
@@ -7,26 +7,71 @@
 // From: https://www.w3resource.com/cpp-exercises/basic/index.php
 */
 
-// Write a C++ program to convert temperature in Celsius to Fahrenheit. 
-int convert_celsius_fahrenheit()
+// Write a C++ program to convert temperature in Celsius to Fahrenheit.
+// From: https://www.sanfoundry.com/cpp-program-perform-celsius-fahrenheit-conversion/
+int convert_celsius_to_fahrenheit()
 {
+    // formula °F = (°C × 1.8) + 32
+    using namespace std;
+
+    float fahrenheit, celsius;
+    
+    cout << "Enter the temperature in Celsius: ";
+    cin >> celsius;
+
+    fahrenheit = (celsius * 9/5) + 32;
+
+    cout << "The temperature in Celsius: " << celsius << endl;
+    cout << "The temperature in Fahrenheit: " << fahrenheit << endl;
     return 0;
 }
 
-// Write a C++ program to find the total number of minutes between two given times (formatted with a colon and am or pm).
-int number_minutes_two()
+int convert_fahrenheit_to_celsius()
 {
+    // formula C = (F − 32) * 9/5​ (1.8)
+    float fahrenheit, celsius;
+
+    std::cout << "Enter the temperature in Fahrenheit: ";
+    std::cin >> fahrenheit;
+
+    celsius = (fahrenheit - 32) * 9/5;
+
+    std::cout << "The temperature in fahrenheit: " << fahrenheit << std::endl;
+    std::cout << "The temperature in celsius: " << celsius << std::endl;
     return 0;
 }
 
 // unit conversion time (second, minute, hour, etc...)
 int unit_conversion_time()
 {
+    float year, week, day, hour, minute, seconds;
+
+    std::cout << "Please, enter the time (in seconds): ";
+    std::cin >> seconds;
+
+    if (seconds < 60)
+    {
+        std::cout << "The time is: " << seconds << " seconds." << std::endl;
+    }
+    else
+    {
+        minute = seconds / 60;
+        hour = seconds / 3600; // 60 minutes in an hour (60 minutes * 60 seconds)
+        day = seconds / 86400; // 24 hours in a day (24 hours * 3600 seconds)
+        week = seconds / 604800; // 7 days in a week (7 days * 86400 seconds)
+        year = seconds / 31536000; // 365 days in a year (365 days * 86400 seconds)
+
+        std::cout << "The minute unit is: " << minute << std::endl;
+        std::cout << "The hour unit is: " << hour << std::endl;
+        std::cout << "The day unit is: " << day << std::endl;
+        std::cout << "The week unit is: " << week << std::endl;
+        std::cout << "The year unit is: " << year << std::endl;
+    }
     return 0;
 }
 
 int main()
 {
-    std::cout << "hhh\n";
+    convert_fahrenheit_to_celsius();
     return 0;
 }
