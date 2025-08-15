6457ab1f113edfee3fadb4b70f11c387e9640404
presidentsnow <xaviyu2005@gmail.com>
2025-08-15 18:29:26 +0200

    Basic pointer practice done.

--- array_pointers_loops_cpp/pointers_test.cpp
+++ array_pointers_loops_cpp/pointers_test.cpp
@@ -4,40 +4,39 @@
 
 void test_pointers()
 {
-    /*int i = 5;
-    int* x = &i;
-    std::cout << i << "\n";
-    std::cout << &i << "\n"; // output the memory address
-    std::cout << *x << "\n";
-    *x = 10;
-    std::cout << *x << "\n";
-    std::cout << i << "\n";*/
+    int A = 10;
+    int *J = &A; // j pointer (*) to the a address (&)
+    int B = *J;
 
-    std::string pizza = "pussy is the best LOL";
-    std::string* name = &pizza;
-    std::cout << name << " " << *name << " ";
-    std::cout << "\n";
+    std::cout << "The value of A is: " << A << "\n";
+    std::cout << "The address of the A is: " << &A << "\n";
+    std::cout << "The value that store a address, J is: " << *J << "\n";
 
-    int a = 10;
-    int *j = &a;
-    int b = *j;
-    std::cout << b << "\n";
+    *J = 8; // modified the J (pointer of a address) value
+    std::cout << "The value J was modified: " << *J << "\n";
+    std::cout << "and now, A value was modified: " << A << "\n";
+
+    int V;
+    int *ptr2 = &V;
+    *ptr2 = 7;
+    std::cout << "V = " << V << "\n";
 }
 
 void pointers_strings()
 {
-    /*std::string name = "MILF";
+    std::string name = "BANNANA";
     std::string* house = &name;
 
     std::cout << *house << "\n";
     std::cout << "The address is: " << house << "\n";
-    *house = "I like tits!";
+    *house = "I like pizza!";
     std::cout << *house << "\n";
-    std::cout << name << "\n";*/
+    std::cout << name << "\n";
 }
 
 int main()
 {
+    //pointers_strings();
     test_pointers();
     return 0;
 }
